efb68f8ca625ea380a2f0744fb91310672487117
Nadiale04 <162038699+Nadiale04@users.noreply.github.com>
2024-03-06 00:07:23 +0000

    Solución

---
+++ Problema10.py
@@ -0,0 +1,5 @@
+colores=['Rojo','Verde','Blanco','Negro','Rosa','Amarillo']
+colores.remove(colores[5])
+colores.remove(colores[4])
+colores.remove(colores[0])
+print(colores)
---
+++ Problema11.py
@@ -0,0 +1,4 @@
+lista_original=[1,1,2,3,4,4,5,1]
+resultado=set(lista_original)
+lista_resultado=list(resultado)
+print(lista_resultado)
